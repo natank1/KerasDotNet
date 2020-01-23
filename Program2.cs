cf81576a9ac17b83782fe135b4592df55ed6c85b
Natan Katz <natan.katz@gmail.com>
2020-01-24 00:04:47 +0200

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Numpy;
+using NumSharp;
+using Keras;
+using Keras.Models;
+using Keras.Layers;
+
+namespace synth0
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //NDarray xtr = Numpy.np.load("C:\\Users\\natank\\Desktop\\codesynth\\myfile.npy");
+            NDarray xtr = Numpy.np.load("C:\\Natan\\fff\\fff\\NKLA_D_100a2200.npy");
+
+            var model = Model.LoadModel("C:\\Users\\natank\\Desktop\\codesynth\\bbb.h5" );
+            Console.WriteLine("yay");
+            NDarray scores = model.Predict(xtr);
+            
+
+        }
+    }
+}
